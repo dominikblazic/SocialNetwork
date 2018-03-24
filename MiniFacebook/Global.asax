2e793cb6eb9b93aca65f60c5e352bd64dc70032e
Dominik Blazic <dominik.blazic@gmail.com>
2018-03-24 14:31:59 +0100

    Add project files.

---
+++ MiniFacebook/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MiniFacebook.MvcApplication" Language="C#" %>
---
+++ MiniFacebook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MiniFacebook.Startup))]
+namespace MiniFacebook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
