50ac27f760d0874bf813c98f23248ba38031aee3
Dominik Blazic <dominik.blazic@gmail.com>
2018-04-17 00:07:08 +0200

    Added Web API to check weather.

---
+++ MiniFacebook/Controllers/WeatherController.cs
@@ -0,0 +1,28 @@
+﻿using MiniFacebook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiniFacebook.Controllers
+{
+    [Authorize]
+    public class WeatherController : Controller
+    {
+        // GET: Weather
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+
+        public JsonResult GetWeather()
+        {
+            WeatherViewModel weather = new WeatherViewModel();
+            return Json(weather.GetWeatherForecast(), JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ MiniFacebook/Models/WeatherViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace MiniFacebook.Models
+{
+    public class WeatherViewModel
+    {
+        public Object GetWeatherForecast()
+        {
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Zagreb&APPID=5434232852cda768290f76209d814b83&units=metric";
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            return jsonContent;
+        }
+    }
+}
