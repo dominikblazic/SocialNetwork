a5752d5dde37848f6606a9b9cbb956d322f87660
Dominik Blazic <dominik.blazic@gmail.com>
2018-05-02 23:44:50 +0200

    Figuring out some things.

---
+++ MiniFacebook.DataAccess/Migrations/201805022143051_ChangedLike.cs
@@ -0,0 +1,22 @@
+namespace MiniFacebook.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangedLike : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Likes", "Post_Id", c => c.Int());
+            CreateIndex("dbo.Likes", "Post_Id");
+            AddForeignKey("dbo.Likes", "Post_Id", "dbo.Posts", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Likes", "Post_Id", "dbo.Posts");
+            DropIndex("dbo.Likes", new[] { "Post_Id" });
+            DropColumn("dbo.Likes", "Post_Id");
+        }
+    }
+}
--- MiniFacebook.Domena/Models/Post.cs
+++ MiniFacebook.Domena/Models/Post.cs
@@ -18,6 +18,9 @@ namespace MiniFacebook.Domena.Models
         [Required]
         [StringLength(450, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         public string Text { get; set; }
+
         public DateTime PostTime { get; set; }
+        public virtual ICollection<Like> Likes { get; set; }
+
     }
 }
